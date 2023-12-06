38e3558bd2776a12519d9e0614c3ee8a94863e6a
Missael Padilla <lmpadillar@gmail.com>
2023-12-06 15:17:20 -0800

    Create user endpoint

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsEmail()  
+    email: string;
+    @IsString()
+    name: string;
+    @IsString()
+    lastName: string;
+}
